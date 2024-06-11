2480496af0e3868fa8e8a7da639d931fd4fb44aa
danicalai <danicalai@tist.com.tw>
2024-06-11 11:29:32 +0800

    Angular 入門教學(1_3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
+/**
+ * 怎麼串聯檔案，就是在這邊宣告起來的
+*/
 @Component({
   selector: 'app-root',
   standalone: true,
